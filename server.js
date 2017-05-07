4bc3bc355c0ef8bc02b858b20cffcb374f531bf1
Abtin Ravanipour <Abtinr1185@gmail.com>
2017-05-07 16:23:06 -0700

    styling..

--- server.js
+++ server.js
@@ -3,26 +3,21 @@ const app = express();
 const methodOverride = require("method-override");
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const exphbs = require("express-handlebars");
+const connection = require("./config/connection.js");
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
 app.use(bodyParser.json());
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
 //Use handlebars information for formatting
-const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //Import connection from config file and connect to MySQL database.
-const connection = require("./config/connection.js");
 
 // Import router from controller file
 const ROUTER = require("./controllers/burgers_controllers.js");
